e7170709961e445e6ccd9e879395abb8c39c4fac
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 22:48:58 +0300

    Exploring the XMLHttpRequest

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
+// var xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'data.json', true);
+// xhr.responseType = 'text';
+// xhr.send();
+
+// xhr.onreadystatechange = function() {
+//     console.log(xhr.readyState);
+//     console.log(xhr.status);
+//     console.log(xhr.statusText);
+// } // end onreadystatechange function
+
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         var myStuff = JSON.parse(xhr.responseText);
+//         console.log(myStuff);
+//     } // eend if
+// } // end onload function
+// xhr.send();
+
+
+
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'data.json', true);
+xhr.responseType = 'text';
 
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        var myStuff = JSON.parse(xhr.responseText);
+        console.log(myStuff);
+    } // eend if
+} // end onload function
 xhr.send();
-
-xhr.onreadystatechange = function() {
-    console.log(xhr.readyState);
-    console.log(xhr.status);
-    console.log(xhr.statusText);
-}//end function
