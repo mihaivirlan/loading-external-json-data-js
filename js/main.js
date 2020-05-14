2ee977c728e45fddece1224676eac88afdf0306b
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 22:36:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'data.json', true);
+
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    console.log(xhr.readyState);
+    console.log(xhr.status);
+    console.log(xhr.statusText);
+}//end function
